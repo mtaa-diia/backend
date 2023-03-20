0a9dbace702cb0ea9ea546a81899ce2e9e5fb0fd
Illia Ponomarov <chippaxyucac@gmail.com>
2023-03-20 21:03:27 +0100

    Database schema and models were created.

---
+++ src/main/java/com/doklad/api/BackendDijaApplication.java
@@ -0,0 +1,13 @@
+package com.doklad.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDijaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendDijaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/doklad/api/BackendDijaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doklad.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDijaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
